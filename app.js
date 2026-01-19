9d9642af38880371733c812f572836599123d8c0
Ayushhrajput <ayushrajput939@gmail.com>
2026-01-19 15:12:22 +0530

    game map

---
+++ app.js
@@ -0,0 +1,8 @@
+const layout = document.querySelector('.layout')
+const width = 80;
+const height = 40;
+
+for(let idx=0; idx<width*height; idx++){
+    const square = document.createElement('div')
+    layout.appendChild(square)
+}
